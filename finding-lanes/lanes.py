75d4f396446d5842d073b69ab87661fb5c353151
Aaron Spindler <nova1104@live.com>
2020-02-07 23:45:27 -0500

    Added lines on colour image

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -13,11 +13,22 @@ def canny(image):
     canny = cv2.Canny(blur, 50, 150)
     return canny
 
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+
+    return line_image
+
 def region_of_interest(image):
     height = image.shape[0]
     polygons = np.array([[(200, height), (1100, height), (550, 250)]])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons, 255)
+
+    # Filters out any lines outside of the enclosing polygon mask
     masked_image = cv2.bitwise_and(image, mask)
     return masked_image
 
@@ -27,6 +38,9 @@ image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 canny = canny(lane_image)
 cropped_image = region_of_interest(canny)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+line_image = display_lines(lane_image, lines)
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
 
-cv2.imshow('results', cropped_image)
+cv2.imshow('results', combo_image)
 cv2.waitKey(0)
