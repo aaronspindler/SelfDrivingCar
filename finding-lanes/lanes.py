b70a74dc0f9adb148cdaecc8618ca6751c3091f9
Aaron Spindler <nova1104@live.com>
2020-02-07 22:57:55 -0500

    Apply canny to image to find changes in brightness with a gradient

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -3,8 +3,18 @@ import numpy as np
 
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
+
+# Step 1 Make image grayscale so there is only 1 channel instead of 3
 gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
 
+# Step 2 Reduce noise
+blur = cv2.GaussianBlur(gray, (5,5), 0)
 
-cv2.imshow('results', gray)
+# Step 3 Find the gradient
+canny = cv2.Canny(blur, 50, 150)
+
+
+
+
+cv2.imshow('results', canny)
 cv2.waitKey(0)
